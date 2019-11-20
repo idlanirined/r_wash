c00cee8231fdcae652ad0ec2ae98c51ac5602ed6
Deni Rinaldi <denirinaldi13@gmail.com>
2019-11-20 11:02:22 +0700

    Account & History (Done)

--- App/Container/Account.js
+++ App/Container/Account.js
@@ -1,11 +1,38 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
+import { Fonts } from '../Themes'
+import { Avatar } from 'react-native-elements'
+import Fontisto from 'react-native-vector-icons/Fontisto'
+import Images from '../Lib/Images'
 
 class Account extends Component {
     render(){
         return(
-            <View>
-                <Text>Screen Account</Text>
+            <View style={{ flex: 1}}>
+                <View style={{ height: 120, width: '100%', backgroundColor: '#32d7e3'}}>
+                </View>
+                <View style={{ paddingHorizontal: 20, marginTop: -80}}>
+                    <Text style={{ fontFamily: Fonts.type.regular, fontSize: 20, alignSelf : 'center', color: 'white'}}>Account</Text>
+
+                    {/* Account */}
+                    <View style={{ backgroundColor:'white' , width: '100%', height: 140, borderRadius: 15, marginTop: 5, paddingVertical: 5}}>
+                        <Avatar rounded source={Images.avatar} containerStyle={{ alignSelf: 'center' }} size='large'/>
+                        <Text style={{ fontFamily: Fonts.type.bold, marginTop: 10, alignSelf: 'center'}}>HAMJAH HUAILID</Text>
+                        <Text style={{ fontFamily: Fonts.type.light, alignSelf: 'center', fontSize: 12}}>082240206592</Text>
+                    </View>
+
+                    {/* Profile */}
+                    <View style={{ backgroundColor:'white' , width: '100%', height: 60, borderRadius: 15, marginTop: 10, paddingVertical: 5, flexDirection: 'row', paddingHorizontal: 20}}>
+                        <Image source={Images.iconAccount} style={{width: 20, height: 20, resizeMode: 'contain', alignSelf: 'center'}}/>
+                        <Text style={{ alignSelf: 'center', marginLeft: 20, fontFamily: Fonts.type.medium}}>Profile</Text>
+                    </View>
+
+                    {/* Payment */}
+                    <View style={{ backgroundColor:'white' , width: '100%', height: 60, borderRadius: 15, marginTop: 10, paddingVertical: 5, flexDirection: 'row', paddingHorizontal: 20}}>
+                        <Fontisto name='wallet' size={20} style={{ alignSelf: 'center' }} color={'gray'} />
+                        <Text style={{ alignSelf: 'center', marginLeft: 20, fontFamily: Fonts.type.medium}}>Payment</Text>
+                    </View>
+                </View>
             </View>
         )
     }
--- App/Container/History.js
+++ App/Container/History.js
@@ -1,11 +1,55 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView } from 'react-native'
+import { Fonts } from '../Themes'
+import Ionicons from 'react-native-vector-icons/Ionicons'
 
 class History extends Component {
+    constructor(props){
+        super(props)
+        this.state={
+            arrayDummy: [
+                { tempat: 'Dipatiukur', jalan: 'Jl. Dipatiukur No.5 Bandung', waktu: 'THU, 24 October / 13.20.05', mobil: 'Honda Jazz / D 1514 BFF', via: 'Cash', harga:'IDR 25.000,-', status: 'Proccess'},
+                { tempat: 'Dipatiukur', jalan: 'Jl. Dipatiukur No.5 Bandung', waktu: 'THU, 24 October / 13.20.05', mobil: 'Honda Jazz / D 1514 BFF', via: 'Cash', harga:'IDR 25.000,-', status: 'Cancelled'},
+                { tempat: 'Dipatiukur', jalan: 'Jl. Dipatiukur No.5 Bandung', waktu: 'THU, 24 October / 13.20.05', mobil: 'Honda Jazz / D 1514 BFF', via: 'Cash', harga:'IDR 25.000,-', status: 'Done'},
+                { tempat: 'Dipatiukur', jalan: 'Jl. Dipatiukur No.5 Bandung', waktu: 'THU, 24 October / 13.20.05', mobil: 'Honda Jazz / D 1514 BFF', via: 'Cash', harga:'IDR 25.000,-', status: 'Proccess'},
+                { tempat: 'Dipatiukur', jalan: 'Jl. Dipatiukur No.5 Bandung', waktu: 'THU, 24 October / 13.20.05', mobil: 'Honda Jazz / D 1514 BFF', via: 'Cash', harga:'IDR 25.000,-', status: 'Done'},
+            ]
+        }
+    }
     render(){
         return(
-            <View>
-                <Text>Screen History</Text>
+            <View style={{ flex: 1, backgroundColor: 'white'}}>
+                <View style={{ height: 60, width: '100%', backgroundColor: '#32d7e3', justifyContent: 'center'}}>
+                    <Text style={{ alignSelf: 'center', justifyContent: 'center', fontFamily: Fonts.type.medium, color: 'white', fontSize: 20}}>History</Text>
+                </View>
+                <ScrollView style={{ paddingHorizontal: 20, marginVertical: 10}} showsVerticalScrollIndicator={false} >
+                    {this.state.arrayDummy.map((item, index) => {
+                        return(
+                            <View style={{ marginTop: index == 0 ? 0 : 20}} key={index}>
+                                <View style={{ width: '100%', backgroundColor: '#dedede', borderTopLeftRadius: 15, borderTopRightRadius: 15, paddingHorizontal: 20}}>
+                                    <Text style={{ alignSelf: 'center', marginTop: 10, fontFamily: Fonts.type.bold, fontSize: 16}}>{item.tempat}</Text>
+                                    <Text style={{ fontFamily: Fonts.type.light, marginTop: 10}}>{item.jalan}</Text>
+                                    <Text style={{ fontFamily: Fonts.type.light}}>{item.waktu}</Text>
+                                    <Text style={{ fontFamily: Fonts.type.light}}>{item.mobil}</Text>
+                                    <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+                                        <Text style={{ fontFamily: Fonts.type.light}}>{item.via}</Text>                          
+                                        <Text style={{ fontFamily: Fonts.type.bold}}>{item.harga}</Text>                          
+                                    </View>
+                                </View>
+                                <View style={{ flexDirection: 'row', justifyContent: 'space-between', backgroundColor: '#dedede' }}>
+                                    <View style={{ height: 25, width: 25, backgroundColor: 'white', borderRadius: 20, marginLeft: -10}}></View>
+                                    <View style={{ borderWidth: .3, marginTop: 5, height: '1%', width: '85%', alignSelf: 'center'}} />
+                                    <View style={{ height: 25, width: 25, backgroundColor: 'white', borderRadius: 20, marginRight: -10}}></View>
+                                </View>
+                                <View style={{ backgroundColor: '#dedede', height: 40, paddingHorizontal: 20, flexDirection: 'row', paddingBottom: 10, borderTopColor: '#dedede'}}>
+                                    <Ionicons name='ios-timer' size={28} color={item.status == 'Proccess' ? 'orange' : item.status == 'Cancelled' ? 'red' : 'green'} style={{ alignSelf: 'center'}}/>
+                                    <Text style={{ fontFamily: Fonts.type.bold, alignSelf: 'center', marginLeft: 10, color: item.status == 'Proccess' ? 'orange' : item.status == 'Cancelled' ? 'red' : 'green', fontSize: 16}}>{item.status}</Text>
+                                </View>
+                            </View>
+                        )
+                    })}
+                    
+                </ScrollView>
             </View>
         )
     }
