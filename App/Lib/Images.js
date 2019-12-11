d7097848390058f8c473908491f64f1cf8a3924d
Hamjah <hamjah.huailid18@gmail.com>
2019-12-11 18:21:13 +0700

    Main menu search

--- App/Lib/Images.js
+++ App/Lib/Images.js
@@ -25,6 +25,7 @@ const Images = {
     iconFilter: require('../Assets/Icon/filter&sort.png'),
     iconOvo: require('../Assets/Icon/OVO.png'),
     iconGopay: require('../Assets/Icon/Gopay.png'),
+    iconSearch: require('../Assets/Icon/search.png'),
 
     //img
     splash: require('../Assets/img/Splashscreen.jpg'),
