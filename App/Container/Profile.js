47f36a45c32c9d0d595e04ffbd1bbd68d772f310
novalmf28 <novalfaisal26@gmail.com>
2019-12-13 20:54:34 +0700

    +Filter, update login,account,profile

--- App/Container/Profile.js
+++ App/Container/Profile.js
@@ -14,11 +14,11 @@ class Profile extends Component {
                 <View style={{ height: 120, width: '100%', backgroundColor: '#00CCFF' }}>
                 </View>
                 <View style={{ justifyContent: 'space-between', flex: 1, paddingBottom: 20 }}>
-                    <View style={{ paddingHorizontal: 20, marginTop: -80, elevation: 2 }}>
+                    <View style={{ paddingHorizontal: 15, marginTop: -80, elevation: 2 }}>
                         <Text style={{ fontFamily: Fonts.type.regular, fontSize: 20, alignSelf: 'center', color: 'white' }}>Profile</Text>
 
                         {/* Account */}
-                        <View style={{ backgroundColor: 'white', width: '100%', height: 140, borderRadius: 5, marginTop: 5, paddingVertical: 5 }}>
+                        <View style={{ backgroundColor: 'white', width: '100%', height: 140, borderRadius: 5, marginTop: 5, paddingVertical: 5, elevation:2 }}>
                             <Avatar rounded source={Images.avatar} containerStyle={{ alignSelf: 'center' }} size='large' />
                             <Text style={{ fontFamily: Fonts.type.bold, marginTop: 10, alignSelf: 'center', fontSize: 16 }}>HAMJAH HUAILID</Text>
                             <Text style={{ fontFamily: Fonts.type.light, alignSelf: 'center', fontSize: 12 }}>082240206592</Text>
@@ -26,7 +26,7 @@ class Profile extends Component {
 
                         {/* Profile */}
 
-                        <View style={{ height: 150, paddingTop: 20, backgroundColor: 'white', marginTop: 10, borderRadius: 5 }}>
+                        <View style={{ height: 150, paddingTop: 20, backgroundColor: 'white', marginTop: 10, borderRadius: 5, elevation:2 }}>
                             <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', position: 'relative' }}>
                                 <Text style={{ alignSelf: 'flex-start', marginLeft: 55, fontFamily: Fonts.type.medium, fontSize: 16 }}>Komplek Bumi Pakusarakan 2</Text>
                                 <Image source={Images.iconHome1} style={{ height: 23, width: 23, position: 'absolute', left: 20 }} />
@@ -44,13 +44,13 @@ class Profile extends Component {
                         </View>
 
                         {/* Vehicle */}
-                        <View style={{ backgroundColor: 'white', width: '100%', height: 60, borderRadius: 5, marginTop: 10, paddingVertical: 5, flexDirection: 'row', paddingHorizontal: 20 }}>
+                        <View style={{ backgroundColor: 'white', width: '100%', height: 60, borderRadius: 5, marginTop: 10, paddingVertical: 5, flexDirection: 'row', paddingHorizontal: 20, elevation: 2 }}>
                             <Image source={Images.iconCar} style={{ width: 25, height: 25, resizeMode: 'contain', alignSelf: 'center' }} />
                             <Text style={{ alignSelf: 'center', marginLeft: 10, fontFamily: Fonts.type.medium, fontSize: 16 }}>Vehicle</Text>
                         </View>
 
                         {/* Tampilan Daftar Mobil */}
-                        <ScrollView style={{width: 300, height: 120,paddingTop:10, paddingHorizontal: 10, paddingBottom: 10, marginTop: 10, left: 24, backgroundColor: 'white', marginLeft: -15, marginRight: -15 }}>
+                        <ScrollView style={{width: 300, height: 120,paddingTop:10, paddingHorizontal: 10, paddingBottom: 10, marginTop: 10, left: 24, backgroundColor: 'white', marginLeft: -15, marginRight: -15, elevation:2, borderRadius: 5 }}>
                        
                             <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', position: 'relative' }}>
                                 <Text style={{ alignSelf: 'flex-start', marginLeft: 35, fontFamily: Fonts.type.medium, fontSize: 16 }}>1. Honda Jazz / D 1514 BFF</Text>
