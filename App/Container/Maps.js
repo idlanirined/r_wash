b4b7a587fbe8dd230c7dddf667fc63fd10350b4d
novalmf28 <novalfaisal26@gmail.com>
2020-01-16 18:03:21 +0700

    ++

--- App/Container/Maps.js
+++ App/Container/Maps.js
@@ -13,6 +13,7 @@ import { request, PERMISSIONS } from 'react-native-permissions';
 import MapView, { PROVIDER_GOOGLE, Marker, Polygon, Callout } from 'react-native-maps';
 import Geolocation from '@react-native-community/geolocation';
 import Icon from 'react-native-vector-icons/Entypo';
+import Modal from 'react-native-modal'
 let myMap;
 
 //const [marker, setMarker] = useState ({})
@@ -22,16 +23,17 @@ class Maps extends Component {
     constructor(props) {
         super(props)
         this.state = {
+            modalbooking: false,
             mobil: 'Honda Jazz',
             search: '',
             markers: [
                 { pin: Images.iconpinrwash2}
             ],
             coordinates: [
-                { latitude: -6.897774, longitude: 107.613805, title: 'R*Wash Dipatiukur', desc: '7 min', src: Images.rwashimage },
-                { latitude: -6.899035, longitude: 107.620709, title: 'Goten Wash', desc: '12 min', src: Images.goten },
-                { latitude: -6.902092, longitude: 107.615473, title: 'Learning Clean', desc: '20 min', src: Images.rwashimage },
-                { latitude: -6.904738, longitude: 107.588264, title: 'Car Wash', desc: '23 min', src: Images.rwashimage }
+                { latitude: -6.897774, longitude: 107.613805, title: 'R*Wash Dipatiukur', desc: '7 min', src: Images.rwashimage, iconpin: Images.iconPin, jarak: '12Km', alamat: 'Jl.Dipatiukur No.5 Bandung', notlp: '082127078200', price: 'IDR 25.000,-', booking: 'Booking', slot: 'Slot 1', waktu: '7 min' },
+                { latitude: -6.899035, longitude: 107.620709, title: 'Goten Wash', desc: '12 min', src: Images.goten, iconpin: Images.iconPin, jarak: '17Km' },
+                { latitude: -6.902092, longitude: 107.615473, title: 'Learning Clean', desc: '20 min', src: Images.rwashimage, iconpin: Images.iconPin, jarak: '20Km'},
+                { latitude: -6.904738, longitude: 107.588264, title: 'Car Wash', desc: '23 min', src: Images.rwashimage, iconpin: Images.iconPin, jarak: '25Km' }
             ],
         }
     }
@@ -102,10 +104,27 @@ class Maps extends Component {
     }
 
     renderCarouselItem = ({item}) =>
-        <View style={styles.cardCarousel}>
-        <Text style={styles.cardTitle}>{item.title}</Text>
-        <Image style={styles.cardImage} source={item.src}/>
-        </View>
+    <View style={styles.cardCarousel}>
+    <Text style={styles.cardTitle}>{item.title}</Text>
+    <TouchableOpacity onPress={() => this.props.navigation.navigate('RwashDetail')}> 
+    <Image style={styles.cardImage} source={item.src}/>
+    </TouchableOpacity>
+    <Image style={styles.cardPin} source={item.iconpin}/>
+    <Text style={styles.cardJarak}>{item.jarak}</Text>
+    <Text style={styles.cardAlamat}>{item.alamat}</Text>
+    <Text style={styles.cardNotlp}>{item.notlp}</Text>
+    <Text style={styles.cardPrice}>{item.price}</Text>
+    <Text style={styles.cardBooking}>{item.booking}</Text>
+    <View style={{ width: 150, height: 100, paddingHorizontal: 30, alignContent: 'center', left: -30, elevation: 10, top:-20 }}>
+                    <Text style={styles.cardSlot}>{item.slot}</Text>
+                        <LinearGradient start={{ x: 0, y: 0 }} end={{ x: 0.9, y: 0.5 }} colors={['#00CCFF', '#00CCFF']} style={{ borderRadius: 20, marginVertical: 20, justifyContent: 'flex-end' }}>
+                            <TouchableOpacity style={{ alignItems: 'center', justifyContent: 'center', height: 40 }} onPress={() => this.setState({ modalbooking: true })} >
+                                <Image source={Images.iconwaktu} style={{width:20, height:20, alignSelf:'flex-start', top:-2, left:10}}></Image>
+                                <Text style={styles.cardWaktu}>{item.waktu}</Text>
+                            </TouchableOpacity>
+                        </LinearGradient>
+                    </View>
+    </View>
 
     render() {
 
@@ -144,6 +163,28 @@ class Maps extends Component {
 
 
         return (
+            <View style={{ flex: 1, backgroundColor: 'white' }}>
+            <Modal
+                    onBackdropPress={() => this.setState({ modalbooking: false })}
+                    isVisible={this.state.modalbooking}
+                >
+                    <View style={{ height: 200, width: '100%', backgroundColor: 'white', paddingVertical: 15, paddingHorizontal: 20 }}>
+                        <Text style={{ alignSelf: 'center', fontSize: 24 }}>Booking</Text>
+                        <Text style={{ alignSelf: 'center', flexWrap: 'wrap', marginTop: 10 }}>Slot 1</Text>
+                        <View style={{ justifyContent: 'space-between', flexDirection: 'row', flex: 1, paddingHorizontal: 40, paddingBottom: 30 }}>
+                            <TouchableOpacity style={{ height: 40, width: 70, borderRadius: 10, backgroundColor: '#f2f2f2', opacity: 1, alignSelf: 'flex-end' }}onPress={() => this.props.navigation.navigate('MyOrder')}>
+                                <View style={{ flex: 1, justifyContent: 'center' }}>
+                                    <Text style={{ alignSelf: 'center' }}>Yes</Text>
+                                </View>
+                            </TouchableOpacity>
+                            <TouchableOpacity style={{ height: 40, width: 70, borderRadius: 10, backgroundColor: '#f2f2f2', opacity: 1, alignSelf: 'flex-end' }} onPress={() => this.setState({ modalbooking: false })}>
+                                <View style={{ flex: 1, justifyContent: 'center' }}>
+                                    <Text style={{ alignSelf: 'center' }}>No</Text>
+                                </View>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                </Modal>
             <View style={{ flex: 1 }}>
                 {/* HEADER */}
                 <View style={{ height: 50, width: '100%', backgroundColor: '#00CCFF', flexDirection: 'row', justifyContent: 'space-between' }}>
@@ -204,11 +245,11 @@ class Maps extends Component {
                                     coordinate={{ latitude: item.latitude, longitude: item.longitude }}>
                                     <Image source={Images.iconpinrwash2} style={{width:50, height:50}}/>
                                 
-                                <Callout>       
+                                    <Callout style={{width:100, height:30, alignItems:'center'}}>       
                                     <Text>{item.title}</Text>
                                     <Text>{item.desc}</Text>
                                 </Callout>
-                                
+
                             </Marker>
                             )
                         })}
@@ -246,6 +287,7 @@ class Maps extends Component {
                </View>
 
         </View>
+            </View>
             </View>
             
         )
@@ -297,7 +339,7 @@ const styles = StyleSheet.create({
     carousel: {
         position: 'absolute',
         bottom: 0,
-        marginBottom: 75,
+        marginBottom: 70,
     },
     cardCarousel: {
         backgroundColor: 'white',
@@ -308,15 +350,72 @@ const styles = StyleSheet.create({
     },
     cardImage: {
         height: 100,
-        width:120,
-        marginTop:-15,
+        width:135,
+        marginTop:-20,
         borderRadius: 5,
     },
     cardTitle: {
-        paddingLeft: 160,
-        fontSize: 20
-    }
+        paddingLeft: 155,
+        fontSize: 20,
+        fontFamily: Fonts.type.bold
+    },
+    cardPin: {
+        height: 20,
+        width:20,
+        marginTop: -80,
+        marginLeft: 150
+    },
+    cardJarak: {
+        paddingLeft: 173,
+        fontSize: 14,
+        marginTop:-17,
+        color: 'red',
+        fontFamily: Fonts.type.regular
+    },
+    cardAlamat: {
+        paddingLeft: 155,
+        fontSize: 14,
+        marginTop: 5,
+        color: '#808080',
+        fontFamily: Fonts.type.regular
+    },
+    cardNotlp: {
+        paddingLeft: 155,
+        fontSize: 14,
+        marginTop: 3,
+        color: '#808080',
+        fontFamily: Fonts.type.regular
+    },
+    cardPrice: {
+        paddingLeft: 230,
+        fontSize: 18,
+        marginTop: 7,
+        color: 'black',
+        fontFamily: Fonts.type.bold
+    },
+    cardBooking: {
+        paddingLeft: 130,
+        fontSize: 18,
+        marginTop: 10,
+        color: 'red',
+        fontFamily: Fonts.type.bold
+    },
+    cardSlot: {
+        fontFamily: Fonts.type.bold,
+        paddingLeft: 25,
+        paddingTop:15,
+        marginBottom:-20
+        
+    },
+    cardWaktu: {
+        fontFamily: Fonts.type.bold,
+        paddingLeft: 25,
+        marginTop:-20,
+        color: 'white'
+        
+    },
 })
+
 
 
 export default Maps;
